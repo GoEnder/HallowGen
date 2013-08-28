2764f955771f04936a8cf0a1e4045eca3db16745
Joshua Baldwin <wiikdworld@googlemail.com>
2013-08-28 19:06:51 +0100

    Remove bullshit

--- src/main/java/net/gatesofender/subterranea/GeneratorOptions.java
+++
@@ -1,9 +0,0 @@
-package net.gatesofender.HallowGen;
-
-
-public class GeneratorOptions {
-    public boolean undergroundBiomes;
-    public boolean silverfish;
-    public boolean giantCaves;
-    public String caveSettings;
-}
